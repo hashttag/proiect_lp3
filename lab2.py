79c56b1991db845168a78aca6a13eeda972a863e
bogdan bigu <134151562+qsdgdv@users.noreply.github.com>
2023-05-21 19:09:08 +0300

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,42 @@
+import psutil
+import logging
+import logging
+
+# 1.Monitorizare procent incarcare cpu / RAM
+def display_usage():
+    print(f"CPU: {psutil.cpu_percent(interval=1)} / RAM: {psutil.virtual_memory().percent}")
+
+
+# 2.Atentionare la trecerea pragului
+def check_resource_usage(cpu_limit=50, memory_limit=50):
+    # Obținem informații despre consumul de resurse
+    cpu_usage = psutil.cpu_percent(interval=0.1)
+    memory_usage = psutil.virtual_memory().percent
+
+    # Verificăm dacă limitele sunt atinse și afișăm mesaje de avertizare corespunzătoare
+    if cpu_usage >= cpu_limit:
+        print("Avertisment: Consumul de CPU a depășit limita de", cpu_limit, "%")
+
+    if memory_usage >= memory_limit:
+        print("Avertisment: Consumul de memorie a depășit limita de", memory_limit, "%")
+
+
+# Configurare
+logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# def check_temperature():
+#   psutil.sensors_temperatures()
+#  if psutil.sensors_temperatures()>=100:
+#     logging.ERROR('Depasire de temperatura')
+
+while True:
+    display_usage()
+    check_resource_usage(10, 50)
+    # check_temperature()
+
+   # p = psutil.Process()
+    # blocking
+    #p.cpu_percent(interval=10)
+    # non-blocking (percentage since last call)
+    #p.cpu_percent(interval=None)
+    #logging.ERROR('Depasire de temeperatura')
